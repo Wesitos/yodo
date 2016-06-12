4c1f7a3a155154d77387b5ba9bfefe9a637b4e07
Pedro Palacios Avila <pedro_gpa@hotmail.com>
2016-06-12 08:24:14 -0500

    Arreglado rutas de llaves

--- server/src/app.js
+++ server/src/app.js
@@ -3,6 +3,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
+import {parseJWT} from './auth.js';
 
 const app = express();
 
@@ -11,6 +12,8 @@ app.use(bodyParser());
 app.use(cookieParser());
 
 app.use(compression());
+
+app.use(parseJWT);
 
 
 export default app;
--- server/src/auth.js
+++ server/src/auth.js
@@ -2,8 +2,8 @@ import fs from 'fs';
 import parse from 'express-jwt';
 import jwt from 'jsonwebtoken';
 
-const publicKey = fs.readFileSync('../jwt_key.pub');
-const privateKey = fs.readFileSync('../jwt_key.pub');
+const publicKey = fs.readFileSync('jwt_key.pub');
+const privateKey = fs.readFileSync('jwt_key.pub');
 
 export const parseJWT = parse({secret: publicKey});
 
