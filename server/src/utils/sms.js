875989134e4fb236c9fed1cc822c0ec8f4800a30
Pedro Palacios Avila <pedro_gpa@hotmail.com>
2016-06-11 23:58:55 -0500

    Agregado .babelrc, .eslint.json y sms.js

---
+++ server/src/utils/sms.js
@@ -0,0 +1,19 @@
+import twilio from 'twilio';
+
+const ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID,
+      AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN,
+      FROM_NUMBER = process.env.TWILIO_FROM_NUMBER,
+      client = twilio(ACCOUNT_SID, AUTH_TOKEN);
+
+export function verifyNumber(number, code){
+  const msg = `Tu codigo de verificacion es: ${code}`;
+  return sendSMS(number, msg);
+}
+
+export function sendSMS(number, message){
+  return client.sendMessage({
+    to: number,
+    from: FROM_NUMBER,
+    body: message,
+  });
+}
